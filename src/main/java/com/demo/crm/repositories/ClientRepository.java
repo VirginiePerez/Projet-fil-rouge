69f6dba522eda2c16910851ba20ebb03596aec01
Virginie Perez <p.virgini@gmail.com>
2024-02-03 16:17:51 +0100

    repositories

---
+++ src/main/java/com/demo/crm/repositories/ClientRepository.java
@@ -0,0 +1,8 @@
+package com.demo.crm.repositories;
+
+import com.demo.crm.models.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Integer> {
+    // empty
+}
